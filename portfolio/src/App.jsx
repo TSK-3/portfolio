b82e78f05c0eebf5e6dfbd923f5717bf62bbf6bf
TSK <thekarthiktangirala@gmail.com>
2025-06-20 10:12:00 +0530

    basic detailing done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -10,9 +10,11 @@ function App() {
       <h1>My Portfolio</h1>
       <nav>
         <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <div className='nav_buttons'>
+          <li><button ><a href="#about">About</a></button></li>
+          <li><button ><a href="#projects">Projects</a></button></li>
+          <li><button><a href="#contact">Contact</a></button></li>
+          </div>
         </ul>
       </nav>
     </div>
