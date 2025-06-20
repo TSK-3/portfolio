f221735f424ab67ed0f0c69409385a7a50aff305
TSK <thekarthiktangirala@gmail.com>
2025-06-20 10:11:26 +0530

    changes made

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,7 +6,16 @@ function App() {
 
   return (
     <>
-      
+    <div className='Navbar'>
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
     </>
   )
 }
